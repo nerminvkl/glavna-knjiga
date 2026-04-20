7438cdbff01e2633d8ebfb6a812a96daf0871793
GAJBA Studio <108943540+nerminvkl@users.noreply.github.com>
2026-04-20 17:56:44 +0200

    fix: unix line endings entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,5 @@
-echo '#!/bin/bash' > entrypoint.sh
-echo 'set -e' >> entrypoint.sh
-echo 'python manage.py migrate --noinput' >> entrypoint.sh
-echo 'python manage.py collectstatic --noinput' >> entrypoint.sh
-echo 'exec gunicorn _core.wsgi:application --bind 0.0.0.0:8000 --workers 2 --timeout 300 --keepalive 5' >> entrypoint.sh
-
-git add entrypoint.sh _core/settings.py
-git commit -m "fix: disable browser-reload in prod, increase timeout"
-git push
+#!/bin/bash
+set -e
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+exec gunicorn _core.wsgi:application --bind 0.0.0.0:8000 --workers 2 --timeout 300
